24c1a870e34a956e52520685810ef86c11298efb
RonStrauss <ashbelthor@gmail.com>
2023-08-23 20:10:43 +0300

    enhance UX

--- src/layouts/components/ChapterCard.tsx
+++ src/layouts/components/ChapterCard.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 import Link from "next/link";
 
 interface ChapterCardProps {
   title: string;
-  link: string,
-  course: string
+  link: string;
+  course: string;
 }
 
 const ChapterCard: React.FC<ChapterCardProps> = ({ title, link, course }) => {
   return (
-    <div className="dark:bg-gray-900 text-center rounded-lg shadow-md p-6 m-1">
-      <h4><Link href={`/courses/${course}/${link}`}>{title}</Link></h4>
-    </div>
+    <Link href={`/courses/${course}/${link}`}>
+      <div className="dark:bg-gray-700 dark:hover:bg-gray-500 hover:transition-all motion-reduce:hover:scale-100 hover:bg-slate-100 hover:scale-105 text-center rounded-lg shadow-md p-6 m-1">
+        <h4>{title}</h4>
+      </div>
+    </Link>
   );
 };
 
